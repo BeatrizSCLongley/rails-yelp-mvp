873e94c304e58c23048928674a6df850c48434d6
Beatriz Longley <beatrizsousacosta@gmail.com>
2021-04-13 19:04:48 +0200

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,13 @@
 class Restaurant < ApplicationRecord
+  # When a restaurant is destroyed, all of its reviews must be destroyed as well:
   has_many :reviews, dependent: :destroy
 
+  # A restaurant must have a name, an address and a category
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
-  # Reminder - when I create a form for the user to create an instance
-  # I will need to tell it to fetch the options here
 
+  # A restaurant category must belong to this fixed list:
   # Array in Capital letters = create a constant that will be
   # available outside the model - in the html view files
   CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,11 @@
 class Review < ApplicationRecord
+  # A review must belong to a restaurant:
   belongs_to :restaurant
 
+  # A review must have content and a rating:
   validates :content, :rating, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
-
-   # Rating should be a number between 0 and 5
+  # A review rating must be a number between 0 and 5:
   RATING = [0, 1, 2, 3, 4, 5]
+  validates :rating, inclusion: { in: RATING }, numericality: { only_integer: true }
+  # validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
